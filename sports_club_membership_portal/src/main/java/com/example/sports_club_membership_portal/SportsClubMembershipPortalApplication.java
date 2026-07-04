0513e573456b846d4bb8933ba84d1d81ec4223d6
Abdulahi Yasin <mymusic19bbc@gmail.com>
2026-07-04 02:39:03 -1000

    Initial commit

---
+++ sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/SportsClubMembershipPortalApplication.java
@@ -0,0 +1,15 @@
+package com.example.sports_club_membership_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsClubMembershipPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsClubMembershipPortalApplication.class, args);
+	}
+
+
+
+}
---
+++ sports_club_membership_portal/src/test/java/com/example/sports_club_membership_portal/SportsClubMembershipPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sports_club_membership_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsClubMembershipPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
