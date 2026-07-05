d9421ccfb5f0b4c035518f4c98dba419883129ff
abdulahimire813-png <abdulahimire813@gmail.com>
2026-07-05 03:07:20 -1000

    Delete sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/exception directory

--- sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.sports_club_membership_portal.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
