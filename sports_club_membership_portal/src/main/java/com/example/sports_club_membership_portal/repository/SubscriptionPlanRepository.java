e48f62349b12a401530d4c1079e9f04911d0a8c3
abdulahimire813-png <abdulahimire813@gmail.com>
2026-07-05 03:05:00 -1000

    Delete sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/repository directory

--- sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/repository/SubscriptionPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sports_club_membership_portal.repository;
-
-import com.example.sports_club_membership_portal.entity.SubscriptionPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Integer> {
-}
